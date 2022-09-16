0f729be99789c8395a6ed4d2f3f8fc6b752aab68
Suhas Sangangire <suhassanagangire2601@gmial.com>
2022-09-16 11:38:05 +0530

    update and add setup

---
+++ setup.py
@@ -0,0 +1,64 @@
+import os
+from setuptools import setup
+#read
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+if __name__ == "__main__":
+    setup(name="ehtim",
+
+          version = "1.2.4",
+
+          author = "Suhas Sangangire",
+          author_email = "suhassangangire2601@gmail.com",
+          description = "Imaging, analysis, and simulation software for radio interferometry",
+          long_description=read('README.rst'),
+          license = "GPLv3",
+          keywords = "imaging astronomy EHT polarimetry",
+          url = "https://github.com/suhassangangire/Quantum-Sparcle ",
+          download_url = "https://github.com/suhassangangire/Quantum-Sparcle",
+          packages = ["ehtim",
+                      "scripts",
+                      "ehtim.calibrating",
+                      "ehtim.imaging",
+                      "ehtim.io",
+                      "ehtim.modeling",
+                      "ehtim.observing",
+                      "ehtim.plotting",
+                      "ehtim.features",
+                      "ehtim.scattering",
+                      "ehtim.statistics"],
+                      
+          scripts=["scripts/calibrate.py",
+                   "scripts/cleanup.py",
+                   "scripts/cli_blur_comp.py",
+                   "scripts/gendata.py",
+                   "scripts/imaging.py",
+                   "scripts/imgsum.py",
+                   "scripts/verify_gradients.py"],
+
+          install_requires=["numpy",
+                            "scipy",
+                            "astropy",
+                            "matplotlib",
+                            "ephem",
+                            "h5py",
+                            "pandas",
+                            "requests",
+                            "future"
+                          # optional (but recommended) libraries required for some features
+                          # ,"pynfft",   # optional (but highly recommended)
+                          # "networkx", # optional, only needed if using image_agreements()
+                          # "requests", # optional; only needed if using dynamical imaging
+                          # "scikit-image", #optional; only needed for certain image analysis functions  
+                          ],
+          classifiers=[
+            'Development Status :: 3 - Alpha',     
+            'Intended Audience :: Developers',    
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python :: 3.8',
+          ],
+
+         )
+
